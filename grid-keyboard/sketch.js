113cf85f3028233c82f3e642782963473877a9e4
brennnnan <brennan.mackay@sbcglobal.net>
2016-09-24 11:13:19 -0700

    made code more modular, removed unnecessary variables

--- grid-keyboard/sketch.js
+++ grid-keyboard/sketch.js
@@ -1,118 +1,104 @@
-vals = new Array(1000);
+swellValues = new Array(1000);
 dotarray = [];
-var counter = 0;
-var n = 0;
 var offset = 120;
-var stops = 0;
 var mul = 20;
 var firstoct = [65, 69, 73, 78, 82, 87, 92, 98, 104, 110, 117, 123];
 var allkeys = [];
-var cx, cy, cw;
+var canvasX, canvasY, buttonWidth;
 var lines = [];
 var button;
 
 function setup() {
 
-
+  // if window is big enough create canvas with big button, else smaller button
   if (windowWidth > 900 && windowHeight > 700) {
     createCanvas(windowWidth, windowHeight);
     offset = (windowWidth - 660) / 2
-    cx = windowWidth;
-    cy = windowHeight;
-    cw = 100;
+    canvasX = windowWidth;
+    canvasY = windowHeight;
+    buttonWidth = 100;
   } else {
     createCanvas(900, 700);
-    cx = 900;
-    cy = 700;
-    cw = 60;
+    canvasX = 900;
+    canvasY = 700;
+    buttonWidth = 60;
   }
   imageMode(CENTER);
   noStroke();
 
-  var ind = 0;
-  var m = 10;
-  var d = -.4;
-  // fill array that determines circle swell width
-  for (var g = 0; g < 1000; g++) {
-    n = (cos(2 * (pow(d, 2))) + sin(20 * (pow(d, 3))));
-    vals[g] = (n + .28) * 0.90292494784;
-    if (vals[g] < m) {
-      m = vals[g];
-      ind = g;
-    }
-    d += .00105;
-  }
-  vals[1000] = 0;
+  fillSwellArray();
+  makeDots();
 
+  button = new dot(-10, canvasX / 2, canvasY - buttonWidth, buttonWidth, 0, 0, 0)
+}
+
+function makeDots() {
   //Make the dots
   //keep track of which frequencies are being grabbed
   freqcounter = 0
+  counter = 0;
   for (var i = 0; i < 8; i++) {
     for (var j = 0; j < 12; j++) {
       allkeys.push(firstoct[j] * (pow(2, i)));
-      dotarray.push(new dot(counter, (j * 60) + offset, (i + 2) * 60, vals[784] * 20, 784, false, allkeys[freqcounter]));
+      dotarray.push(new dot(counter, (j * 60) + offset, (i + 2) * 60, swellValues[784] * 20, 784, false, allkeys[freqcounter]));
       counter++;
       freqcounter++;
     }
     freqcounter -= 5;
   }
-
-  fill('rgba(10,100,200,.8');
-  var g = 30;
-  button = new dot(-10, cx / 2, cy - cw, cw, 0, 0, 0)
-
 }
 
-function draw() {
+function fillSwellArray() {
+  var d = -.4;
+  var n = 0;
+  // fill array that determines circle swell width
+  for (var g = 0; g < 1000; g++) {
+    n = (cos(2 * (pow(d, 2))) + sin(20 * (pow(d, 3))));
+    swellValues[g] = (n + .28) * 0.90292494784;
+    d += .00105;
+  }
+  swellValues[1000] = 0;
+}
 
-  stops = 0;
-  background('#0E767B');
-  fill('#17BEBB');
-
-  for (var c = 0; c < counter; c++) {
+function drawDots() {
+  for (var c = 0; c < dotarray.length; c++) {
     if (dotarray[c].clicked) {
       //assigning width and amp to each dot if it has been click
       dotarray[c].index += 1;
       num = ((dotarray[c].index) % (1000));
-      dotarray[c].width = vals[num] * mul;
+      dotarray[c].width = swellValues[num] * mul;
       dotarray[c].vibrator.amp(dotarray[c].width / 2)
     }
-
     ellipse(dotarray[c].xctr, dotarray[c].yctr, dotarray[c].width, dotarray[c].width);
-
   }
+}
+
+function drawButton() {
   fill('#D4F4DD');
   stroke(51);
   strokeWeight(1);
   ellipse(button.xctr, button.yctr, button.width, button.width);
   noStroke();
+}
 
-  for (var dd = 0; dd < lines.length; dd++) {
-    strokeWeight(1);
-    stroke(51)
-    line(lines[dd].x1, lines[dd].y1, lines[dd].x2, lines[dd].y2);
-    noStroke();
-    if (lines[dd].v === 0) {
-      lines[dd].x1--;
-      lines[dd].y2++;
-    } else {
-      lines[dd].x2++;
-      lines[dd].y1++;
-    }
-  }
+function draw() {
+  background('#0E767B');
+  fill('#17BEBB');
+
+  drawDots();
+  drawButton();
 
 }
 
 function mouseClicked() {
 
-  for (var f = 0; f < counter; f++) {
+  for (var f = 0; f < dotarray.length; f++) {
     hit = collidePointCircle(mouseX, mouseY, dotarray[f].xctr, dotarray[f].yctr, dotarray[f].width);
 
     if (hit) {
       if (dotarray[f].clicked == true) {
         dotarray[f].clicked = false;
         dotarray[f].vibrator.amp(0, .2);
-        //console.log("turning off");
         return;
       }
       dotarray[f].clicked = true;
@@ -127,13 +113,12 @@ function mouseClicked() {
 }
 
 function mousePressed() {
-  hit = collidePointCircle(mouseX, mouseY, cx / 2, cy - cw, cw);
+  hit = collidePointCircle(mouseX, mouseY, canvasX / 2, canvasY - buttonWidth, buttonWidth);
   if (hit) {
     button.clicked = 1;
     button.width -= 10;
   }
 }
-
 
 function mouseReleased() {
   if (button.clicked == 1) {
@@ -142,13 +127,11 @@ function mouseReleased() {
     for (var ii = 0; ii < dotarray.length; ii++) {
       if (dotarray[ii].vibrator != 0) dotarray[ii].vibrator.stop();
 
-      dotarray[ii].width = vals[784] * 20;
+      dotarray[ii].width = swellValues[784] * 20;
       dotarray[ii].clicked = 0;
     }
   }
 }
-
-
 
 function dot(_id, _xctr, _yctr, _width, _index, _clicked, _osc) {
   this.id = _id;
