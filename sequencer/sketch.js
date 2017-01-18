e68424cc288a02293c7326c59f9350a5fc5a8475
brennnnan <brennan.mackay@sbcglobal.net>
2017-01-18 15:30:45 -0800

    Added bpm control

--- sequencer/sketch.js
+++ sequencer/sketch.js
@@ -7,6 +7,8 @@ var hhc, hho, kck, snr, lt, ht, clp;
 var started=0;
 var ticker =0;
 myPart = new p5.Part();
+var bpm = 110;
+var bpmLock = 0;
 
 var multiplier=20;
 var patterns = new Array(7);
@@ -28,11 +30,9 @@ function preload() {
 
 
 function setup() {
-  createCanvas(1460, 900);
-  imageMode(CENTER);
-  background(255);
-  noStroke();
+  createCanvas(1300, 800);
   
+  rectMode(CENTER)
   //Fills bounce array with values used to show circles swelling
   loadbounce();
   
@@ -47,10 +47,10 @@ function setup() {
   }
   
   fill('rgba(10,100,200,.8');
-  makephrases(110);
+  makephrases();
 }
 //Assigns patterns to 'phrases' and creates a part composed of all phrases.
-function makephrases(bpm){
+function makephrases(){
   myPhrase1 = new p5.Phrase('hihatc', makeSound, patterns[0]); 
   myPhrase2 = new p5.Phrase('hihato', makeSound2, patterns[1]); 
   myPhrase3 = new p5.Phrase('snare', makeSound3, patterns[2]);
@@ -67,7 +67,7 @@ function makephrases(bpm){
   myPart.addPhrase(myPhrase5);
   myPart.addPhrase(myPhrase6);
   myPart.addPhrase(myPhrase7);
-  myPart.setBPM(bpm);
+  myPart.setBPM(110);
   myPart.onStep(grow); 
 }
 
@@ -86,7 +86,7 @@ function grow() {
 }
 
 function loadbounce(){
-  //an array of triangle-like values for 'growing'
+  //creates an array of ~ like values for 'growing' the steps
   var k=multiplier*2;
   for(var u=1;u<=10;u++){
     bounce.push(k+u);
@@ -98,24 +98,21 @@ function loadbounce(){
 
 
 function draw() {
-  background('rgba(64,224,208,1)');
-  //Draw reset rectangle
-  strokeWeight(1);
-  stroke(51);
-  rect(500, 50, 200, 70, 20);
+  background('#8CD6DE');
+  newbpm = round(map(mouseX, 0,1400,80,170))
+  if (newbpm != bpm && bpmLock == 0) {
+    myPart.setBPM(newbpm);
+    bpm = newbpm;
+  }
   noStroke();
-  textSize(32);
-  fill('rgb(60,130,200)');
-  text("reset dots", 530, 95);
-  
-  fill('rgb(173,255,204)');
+  fill('#948CDE');
   
   for (var c=0;c<dot_count;c++){
     
     // if dot has been clicked draw border
     if(dot_array[c].clicked){
       strokeWeight(6);
-      stroke('rgb(255,255,255)');
+      stroke(255,255,255);
     }
     
     // if dot is growing, then grow it according to where it is in array
@@ -128,9 +125,27 @@ function draw() {
         dot_array[c].index=1;
       }
     }
-    ellipse(dot_array[c].xctr,dot_array[c].yctr,dot_array[c].width,dot_array[c].width);
+    
+    dot_array[c].sketch()
     noStroke();
   }
+  
+  textSize(32);
+  fill('rgb(60,130,200)');
+  text('bpm: '+bpm, 530, 95);
+  
+  /*
+  for(var d=0; d<8; d++) {
+    stroke(0)
+    strokeWeight(1)
+    line(120,127+(d*85),windowWidth-200,(0,127+(d*85)))
+  }
+  
+  for(var r=0; r<17; r++) {
+    stroke(0)
+    strokeWeight(1)
+    //line(105+(r*70),40,105+(r*70),800);
+  }*/
 }
 
 function clearPatterns(){
@@ -185,20 +200,23 @@ function mouseReleased() {
 
 function mouseClicked() {
   
-  if(started==0){
-    myPart.start();
-    myPart.loop();
-    started++;
+  
+
+  if (keyCode == OPTION && keyIsPressed == true) {
+    if(bpmLock == 0) bpmLock = 1;
+    else bpmLock = 0;
+    return;
   }
-  // if reset button is clicked
-  hit = collidePointRect(mouseX,mouseY,500, 50, 200, 70);
-  if(hit) {
-    clearPatterns();
-  }
+
   // if any of the dots are clicked, changed .clicked value to true or false if they already are 
   for(var f=0; f<dot_count; f++){
     hit = collidePointCircle(mouseX, mouseY, dot_array[f].xctr, dot_array[f].yctr, dot_array[f].width);
     if (hit) {
+      if(started==0){
+        myPart.start();
+        myPart.loop();
+        started++;
+      }
       // if clicked, indicate it in pattern
       patterns[dot_array[f].colid][dot_array[f].rowid] = (patterns[dot_array[f].colid][dot_array[f].rowid]+1)%2;
       // if turned on, turn off
@@ -209,6 +227,9 @@ function mouseClicked() {
       dot_array[f].clicked=true;
     }
   }
+  
+  
+  
   
 }
 
@@ -222,4 +243,8 @@ function dot(_id, _rowid, _colid, _xctr, _yctr, _width, _index, _clicked){
   this.index=_index;
   this.clicked=_clicked;
   this.growing=0;
+  
+  this.sketch = function() {
+    rect(this.xctr,this.yctr,this.width,this.width,10);
+  }
 }
